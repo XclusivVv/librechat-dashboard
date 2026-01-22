b68e1ecbe23c364bef7d6eb34d21efc17099fae7
XclusivVv <46164651+XclusivVv@users.noreply.github.com>
2026-01-22 18:03:23 +0000

    Change copyright holder and license terms
    Updated copyright information and licensing terms.

--- librechat-dashboard.py
+++ librechat-dashboard.py
@@ -4,20 +4,12 @@ LibreChat Control Panel v3 - Dashboard Edition
 A modern dashboard GUI for managing LibreChat services on CachyOS
 with real-time monitoring, graphs, and enhanced UX
 """
-# Copyright (c) 2026 <your name or handle>
+# Copyright (c) 2026 XclusivVv
+# All rights reserved.
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# This file is part of a proprietary project.
+# Redistribution, re hosting, or republication is prohibited.
+# See the LICENSE file in the project root for details.
 
 import sys
 import subprocess
